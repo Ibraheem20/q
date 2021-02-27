bc40fc738f9ad2aac26b4cf9a84c0d1f7844f51c
Ahmad <ahamadneh92@gmail.com>
2021-02-27 23:57:28 +0200

    update

---
+++ SE/resources/views/Ahmad.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('styles')
+    <style>
+
+    </style>
+@endsection
+@section('content')
+    <div>
+        <h1>ahmad hamadneh</h1>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+
+    </script>
+@endsection
